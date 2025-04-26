6f589ab32f7f17c8f28bbcf8beddddb9d7753b7c
hannaHk0 <hannahuber1000@outlook.de>
2025-04-26 09:26:25 +0200

    Projekt hinzugefügt

---
+++ Projekt/src/main/java/org/example/projekt/StartupBeam.java
@@ -0,0 +1,22 @@
+
+
+
+package org.example.projekt;
+
+import jakarta.annotation.PostConstruct;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.GameInputDto;
+import org.springframework.stereotype.Component;
+import org.openapitools.client.model.GameDto;
+
+@Component
+public class StartupBeam {
+    @PostConstruct
+    public void init(){
+        DefaultApi defaultApi = new DefaultApi();
+        GameInputDto gameInput = new GameInputDto();
+        gameInput.setGroupName ("zgediu");
+
+        GameDto response = defaultApi.gamePost(gameInput);
+    }
+}
